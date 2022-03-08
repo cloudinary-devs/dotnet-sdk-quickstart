7d07fb592834b1ddaf328bf6e0d7d12f91faf554
Nick Bradley <nick.bradley@cloudinary.com>
2022-03-08 11:40:30 +0000

    initial commit

---
+++ net_quickstart/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+//using dotenv.net;
+
+namespace net_quickstart
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+
+            //Config
+
+            //DotEnv.Load();
+            //Cloudinary cloudinary = new Cloudinary(Environment.GetEnvironmentVariable("CLOUDINARY_URL"));
+            Cloudinary cloudinary = new Cloudinary("<cloudinary_url>");
+
+            //Upload
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(@"https://res.cloudinary.com/demo/image/upload/cld-sample.jpg"),
+                PublicId = "Testing"
+            };
+            var uploadResult = cloudinary.Upload(uploadParams);
+            Console.WriteLine(uploadResult.JsonObj);
+
+            //Admin
+
+            var getResourceParams = new GetResourceParams("Testing")
+            {
+                QualityAnalysis = true
+            };
+            var getResourceResult = cloudinary.GetResource(getResourceParams);
+            Console.WriteLine(getResourceResult.JsonObj);
+
+            //Transformation
+
+            var myTransformation = cloudinary.Api.UrlImgUp.Transform(new Transformation()
+                .Width(150).Crop("scale").Chain()
+                .Effect("cartoonify"));
+
+            var myUrl = myTransformation.BuildUrl("Testing");
+            var myImageTag = myTransformation.BuildImageTag("Testing");
+
+            Console.WriteLine(myUrl);
+            Console.WriteLine(myImageTag);
+        }
+    }
+}
